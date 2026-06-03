815e576765ecc10a66c97ea31ec5dee54ee02e26
kk16-crypto <kk16671@nauka.panschelm.edu.pl>
2026-06-03 18:54:38 +0200

    feat: change list for string

--- ocr/config.py
+++ ocr/config.py
@@ -1,7 +1,6 @@
-TESSERACT_CONFIG = [
-    "stdout --oem 3 --psm 8 -c tessedit_char_whitelist=0123456789.,",
-]
-DEFAULT_TARGET_COLOR = (0, 0, 0)
+TESSERACT_CONFIG = "stdout --oem 3 --psm 8 -c tessedit_char_whitelist=0123456789.,",
+
+DEFAULT_TARGET_COLOR = (200, 200, 200)
 DEFAULT_TOLERANCE = 10
 DEFAULT_SCALE = [3.45, 3.41]
 DEFAULT_PADDING = 15
